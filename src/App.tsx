2f80b51f268cf7f512df62b16efcfd4a882d959a
Matheus Augusto Sabino Ferraz <matheusaugustosabinoferraz@outlook.com>
2026-08-06 21:21:15 -0300

    feat: wire collaborators and account routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import { Toaster } from 'sonner'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import Register from './pages/Register'
+import Colaboradores from './pages/Colaboradores'
+import MinhaConta from './pages/MinhaConta'
 import { Layout } from './components/layout/Layout'
 import { RequireAuth } from './components/layout/RequireAuth'
+import { RequireAdmin } from './components/layout/RequireAdmin'
 
 function App() {
   return (
@@ -15,7 +18,11 @@ function App() {
         <Route element={<RequireAuth />}>
           <Route element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="register" element={<Register />} />
+            <Route path="minha-conta" element={<MinhaConta />} />
+            <Route element={<RequireAdmin />}>
+              <Route path="colaboradores" element={<Colaboradores />} />
+              <Route path="register" element={<Register />} />
+            </Route>
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Route>
